bd4f450f2cacaa8832f1921e2b4cb43187994bcf
YuvaneshSankar <yuvanesh.skv@gmail.com>
2025-07-12 00:51:56 +0530

    Added about componenet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from './components/Hero';
+import About from "./components/About";
+
 
 const App = () => {
   return (
     <main className="realtive min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <About />
 
-      <section className="z-0 min-h-screen bg-blue-500"/>
+      {/* <section className="z-0 min-h-screen bg-blue-500"/> */}
     </main>
   )
 }
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -147,7 +147,7 @@ const Hero = () => {
 
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
-            <h1 className="special-font hero-heading text-blue-300">
+            <h1 className="special-font hero-heading text-black">
               Yuva<b>n</b>esh
             </h1>
 
